ed23b6b8fc588478279b2774d8d15244d8895243
Fudgyking <escalado19@hotmail.com>
2019-01-27 21:00:20 -0500

    Restore previous emote-pain behavior

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -709,13 +709,19 @@ bool CCharacter::TakeDamage(vec2 Force, vec2 Source, int Dmg, int From, int Weap
 	if(From == m_pPlayer->GetCID())
 		m_Core.m_Vel += Force;
 
-	if(GameServer()->m_pController->IsFriendlyFire(m_pPlayer->GetCID(), From) || (From == m_pPlayer->GetCID() && !g_Config.m_SvRocketJumpDamage))
+	if(GameServer()->m_pController->IsFriendlyFire(m_pPlayer->GetCID(), From))
 		return false;
+
+	if(From == m_pPlayer->GetCID() && !g_Config.m_SvRocketJumpDamage){
+		m_EmoteType = EMOTE_PAIN;
+		m_EmoteStop = Server()->Tick() + 500 * Server()->TickSpeed() / 1000;
+		return false;
+	}
 
 	// m_pPlayer only inflicts half damage on self
 	if(From == m_pPlayer->GetCID())
 		Dmg = max(1, Dmg/2);
-
+		
 	int OldHealth = m_Health, OldArmor = m_Armor;
 	if(Dmg)
 	{
