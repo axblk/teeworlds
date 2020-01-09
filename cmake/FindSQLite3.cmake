436012b93344d5820058f25b88ad1e4644823656
Redix <redix@hotmail.de>
2020-01-09 02:30:08 +0100

    Fix cmake sqlite3 detection

--- cmake/FindSQLite3.cmake
+++ cmake/FindSQLite3.cmake
@@ -2,6 +2,33 @@ if(NOT PREFER_BUNDLED_LIBS)
   set(CMAKE_MODULE_PATH ${ORIGINAL_CMAKE_MODULE_PATH})
   find_package(SQLite3)
   set(CMAKE_MODULE_PATH ${OWN_CMAKE_MODULE_PATH})
+
+  if(NOT SQLite3_FOUND)
+    find_path(SQLite3_INCLUDEDIR NAMES sqlite3.h)
+    find_library(SQLite3_LIBRARY NAMES sqlite3 sqlite)
+
+    mark_as_advanced(SQLite3_LIBRARY SQLite3_INCLUDEDIR)
+
+    if(SQLite3_INCLUDEDIR)
+      file(STRINGS ${SQLite3_INCLUDEDIR}/sqlite3.h _ver_line
+          REGEX "^#define SQLITE_VERSION  *\"[0-9]+\\.[0-9]+\\.[0-9]+\""
+          LIMIT_COUNT 1)
+      string(REGEX MATCH "[0-9]+\\.[0-9]+\\.[0-9]+"
+            SQLite3_VERSION "${_ver_line}")
+      unset(_ver_line)
+    endif()
+
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(SQLite3
+        REQUIRED_VARS SQLite3_INCLUDEDIR SQLite3_LIBRARY
+        VERSION_VAR SQLite3_VERSION)
+    
+    if(SQLite3_FOUND)
+      set(SQLite3_INCLUDE_DIRS ${SQLite3_INCLUDEDIR})
+      set(SQLite3_LIBRARIES ${SQLite3_LIBRARY})
+    endif()
+  endif()
+
   if(SQLite3_FOUND)
     set(SQLite3_BUNDLED OFF)
     set(SQLite3_DEP)
